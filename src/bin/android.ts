e164b261a822504d4d2c7e6b4917bbe3281e7884
Martin Donadieu <martindonadieu@gmail.com>
2023-01-13 00:37:15 +0800

    fix: upgrade script for standar version

--- src/bin/android.ts
+++ src/bin/android.ts
@@ -1,22 +1,19 @@
 import { versionBetaToCode, versionCodeToCodeBeta, versionPureToCode } from './utils';
 
+const regexM = /versionName\s"(.*)"$/g;
+const regexC = /versionCode\s(.*)$/g;
 export const readVersion = contents => {
-  const version = contents.split('versionName "')[1].split('"')[0];
+  const marketingVersionString = contents.match(regexM);
+  const version = marketingVersionString.replace(regexM, '$1');
   return version;
 };
 
 export const writeVersion = (contents, version) => {
   const [versionPure, versionBeta] = version.split('-');
-  const newContent = contents.replace(
-    /(.*(?:versionName[ \t]+).*)/g,
-    `\t\tversionName "${versionPure}"`
-  );
+  const newContent = contents.replace(regexM, `versionName "${versionPure}"`);
   let versionCode = versionPureToCode(versionPure);
   let versionCodeBeta = versionBetaToCode(versionBeta);
   const versionCodeFinal = versionCodeToCodeBeta(versionCode, versionCodeBeta);
-  const finalContent = newContent.replace(
-    /(.*(?:versionCode[ \t]+).*)/g,
-    `\t\tversionCode ${versionCodeFinal}`
-  );
+  const finalContent = newContent.replace(regexC, `versionCode ${versionCodeFinal}`);
   return finalContent;
 };
--- src/bin/ios.ts
+++ src/bin/ios.ts
@@ -1,23 +1,19 @@
 import { versionBetaToCode, versionCodeToCodeBeta, versionPureToCode } from './utils';
 
-export const readVersion = contents => {
-  const marketingVersionString = contents.match(/MARKETING_VERSION = [0-9]*.[0-9]*.[0-9]*/);
-  const version = marketingVersionString.toString().split('=')[1].trim();
+const regexM = /MARKETING_VERSION\s=\s(.*);/g;
+const regexC = /CURRENT_PROJECT_VERSION\s=\s(.*);/g;
+export function readVersion(contents) {
+  const marketingVersionString = contents.match(regexM);
+  const version = marketingVersionString.replace(regexM, '$1');
   return version;
-};
+}
 
-export const writeVersion = (contents, version) => {
+export function writeVersion(contents, version) {
   const [versionPure, versionBeta] = version.split('-');
-  const newContent = contents.replace(
-    /(.*(?:MARKETING_VERSION[ \t]+).*)/g,
-    `\t\t\t\tMARKETING_VERSION = ${versionPure};`
-  );
+  const newContent = contents.replace(regexM, `MARKETING_VERSION = ${versionPure};`);
   let versionCode = versionPureToCode(versionPure);
   let versionCodeBeta = versionBetaToCode(versionBeta);
   const versionCodeFinal = versionCodeToCodeBeta(versionCode, versionCodeBeta);
-  const finalContent = newContent.replace(
-    /(.*(?:CURRENT_PROJECT_VERSION[ \t]+).*)/g,
-    `\t\t\t\tCURRENT_PROJECT_VERSION = ${versionCodeFinal};`
-  );
+  const finalContent = newContent.replace(regexC, `CURRENT_PROJECT_VERSION = ${versionCodeFinal};`);
   return finalContent;
-};
+}
